67f7a71bf3ec13e24018ec7e9f241587928f351f
Dreamer <christopherlu.pdx@gmail.com>
2019-11-22 18:55:54 -0800

    prints the entire array upon exit

--- smltobig.cpp
+++ smltobig.cpp
@@ -8,39 +8,42 @@ using namespace std;
 int main()
 {
  int ARRAYSIZE[20];
- int i, usrInput, numberIndex, nVal, numberArray[20];
+ int i, usrInput, inputCount, numberIndex, nVal, numberArray[20];
  numberIndex = 0;
  //in a loop that runs for no more than ARRAYSIZE times
   nVal = 0; 
-
+  inputCount = 0;
 for (i = 0; i < 20; i++) 
   {
     cout << "gimme int" << endl;
      //put the number into numberArray
-    cin >> usrInput;
-    cout << "numberIndex is " << numberIndex << endl;  
-     
-    numberIndex++; //increments row position (starting at 0)
+    cin >> usrInput;    
+    inputCount = inputCount + 1; 
     if (usrInput == 0) //if usrInput is 0 gtfo
       {
       break;
       }
-  
-   numberArray[numberIndex] = usrInput;    
-   //if numberarray is positive (how'd you want to increment that)
+    
+    if (usrInput != 0 | usrInput < 0)
+       {
+         numberArray[numberIndex] = usrInput;     
+         numberIndex++; //increments row position (starting at 0)
+      }
+    
     if (numberArray[i] >= 0)
       {
       nVal = nVal + 1;
-      }
-    }
-    cout << "positives: " << nVal << endl;
+      } 
+  }
 
-  cout << "qa print" << endl; 
-//qa to print all values 
-//  for (i = 0; i < 20; i++)
-//  {
-//  cout << numberArray[i] << endl;
-//  }
+  
+  cout << "there are x number of values: ";
+  for ( i = 0; i < inputCount - 1; i++)
+   {
+  //the first char of numberArray is zero   
+   cout << numberArray[i] << ", ";
+   }
+  cout << "Inputcount is " << inputCount << endl;
  cout << "what it will do" << endl;
 return 0;
 }
