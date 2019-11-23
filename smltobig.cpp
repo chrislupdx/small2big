e28b7c8d6509e9c2810750790ddc39d0e031c765
Dreamer <christopherlu.pdx@gmail.com>
2019-11-22 18:11:24 -0800

    nVal counts positive values

--- smltobig.cpp
+++ smltobig.cpp
@@ -8,17 +8,17 @@ using namespace std;
 int main()
 {
  int ARRAYSIZE[20];
- int i, usrInput, numberIndex, numberArray[20];
+ int i, usrInput, numberIndex, nVal, numberArray[20];
  numberIndex = 0;
  //in a loop that runs for no more than ARRAYSIZE times
- for (i = 0; i < 20; i++) 
+  nVal = 0; 
+
+for (i = 0; i < 20; i++) 
   {
     cout << "gimme int" << endl;
-    
      //put the number into numberArray
     cin >> usrInput;
-    cout << "numberIndex is " << numberIndex;
-  
+    cout << "numberIndex is " << numberIndex << endl;  
      
     numberIndex++; //increments row position (starting at 0)
     if (usrInput == 0) //if usrInput is 0 gtfo
@@ -27,16 +27,20 @@ int main()
       }
   
    numberArray[numberIndex] = usrInput;    
-   cout << "ugh" << endl; 
-   }
+   //if numberarray is positive (how'd you want to increment that)
+    if (numberArray[i] >= 0)
+      {
+      nVal = nVal + 1;
+      }
+    }
+    cout << "positives: " << nVal << endl;
 
   cout << "qa print" << endl; 
 //qa to print all values 
-  for (i = 0; i < 20; i++)
-  {
-  cout << numberArray[i] << endl;
-
-  }
+//  for (i = 0; i < 20; i++)
+//  {
+//  cout << numberArray[i] << endl;
+//  }
  cout << "what it will do" << endl;
 return 0;
 }
